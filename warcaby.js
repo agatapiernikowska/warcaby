90418d2fa747e16b9affa57ed5de4244de04f00a
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-06-14 00:22:35 +0200

    dodanie funkcji onlick pod button zacznij gre na ekranie powitalnym

--- warcaby.js
+++ warcaby.js
@@ -1,1 +1,6 @@
 document.getElementById('app').appendChild(createTable());
+
+var welcomeScreen = document.querySelector(".welcome-screen");
+function addName() {
+  welcomeScreen.classList.add("welcome-screen-hide");
+}
