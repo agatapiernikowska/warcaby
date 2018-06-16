65847be55b6fd6a749219fc315be1933901c39ce
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-06-16 16:45:56 +0200

    dodanie timera

--- utils.js
+++ utils.js
@@ -3,12 +3,19 @@ var myButton = document.querySelector('.btn');
 var input = document.getElementsByTagName('input')[0];
 var myUl = document.getElementsByTagName('ul')[0];
 
-
-
-myButton.addEventListener('click', function(){
+myButton.addEventListener('click', function () {
   var newLi = document.createElement('li');
   newLi.textContent = input.value;
   myUl.appendChild(newLi);
   input.value = "";
   welcomeScreen.classList.add("welcome-screen-hide");
 });
+
+// TIMER
+function printDuration() {
+  var cnt = 0;
+  setInterval(function () {
+    cnt += 1;
+    document.getElementById("timer").innerHTML = cnt + " seconds";
+  }, 1000);
+}
--- warcaby.js
+++ warcaby.js
@@ -1,16 +1,15 @@
-
 // create board
 function createBoard() {
   var board = document.getElementById("board");
 
   var z = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-  for (var y=0; y < 10 ; y++) {
+  for (var y = 0; y < 10; y++) {
     var row = document.createElement("div");
     row.className = "row";
     board.appendChild(row);
 
 
-    for (var x=1; x < 11; x++) {
+    for (var x = 1; x < 11; x++) {
       var cell = document.createElement("div");
       cell.id = x.toFixed() + y.toString();
       if ((x + y) % 2) {
